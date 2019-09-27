413f8d273813b119d3f441a78c656521c71e0e38
alb3rtobr <alberto.bustamante.reyes@est.tech>
2019-09-27 11:40:59 +0200

    Extracting common server options

--- mygeodecomposer/scripts/startServer.sh
+++ mygeodecomposer/scripts/startServer.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+COMMON_SERVER_OPTIONS="--locators=locator[10334] --server-port=40404 --max-heap=1G"
+COMMON_SERVER_OPTIONS_G1="--locators=locator[10334] --server-port=40404 --J=-XX:+UseG1GC"
+
 sleep 15
 
 mkdir -p /data/$HOSTNAME
 
 ip=`hostname -I` 
 
-taskset 1 gfsh start server --bind-address=$ip --hostname-for-clients=$ip --locators=locator[10334] --dir=/data/$HOSTNAME/ $@
-# --server-port=40404 --max-heap=1G
+gfsh start server --bind-address=$ip --hostname-for-clients=$ip --dir=/data/$HOSTNAME/ ${COMMON_SERVER_OPTIONS} $@
+
 while true; do
   sleep 10
 done
